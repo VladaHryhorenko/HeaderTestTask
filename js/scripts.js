684e31a876bee5f8cbaa51fae2999f5bb5e59745
VladaHryhorenko <mrsgriggy@gmail.com>
2021-06-14 05:00:50 +0300

    adaptive header

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.burger-icon-wrapper').on('click', () => {
+        $('.mobile-nav').toggleClass('mobile-hidden');
+        $('.hamburger').toggleClass('is-active');
+        $('.logo').toggleClass('logo-is-hidden');
+        $('.navigation-wrapper').toggleClass('align-nav-end');
+    })
+
+    $('.item-services').on('click', () => {
+      $('.services').toggleClass('services-hidden');
+      $('.item-services').toggleClass('item-services-is-active')
+      
+  })
+
+    $("#lang-id").select2({
+      theme: "classic",
+    });
+    $("#mob-lang-id").select2({
+      theme: "classic",
+    });
+    
+  });
